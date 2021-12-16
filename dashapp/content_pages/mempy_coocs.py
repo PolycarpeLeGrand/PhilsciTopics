44e2608e27169b863cc07e799cbdb07fae9d0bf8
PolycarpeLeGrand <tapisserie2@gmail.com>
2021-12-16 12:47:49 -0500

    Fixed coocs to focus on 6

--- dashapp/content_pages/mempy_coocs.py
+++ dashapp/content_pages/mempy_coocs.py
@@ -1,23 +1,24 @@
 from dash import Dash, dcc, html, Input, Output, State, callback
 import dash_bootstrap_components as dbc
 from config import BASE_STORAGE_PATH
-from itertools import chain
-
 import pickle
 
 page_name = 'mempy-coocs'
-COOC_SAMPLES = pickle.load(open(BASE_STORAGE_PATH / 'cooc_samples_dict.p', 'rb'))
+COOC_SAMPLES = pickle.load(open(BASE_STORAGE_PATH / 'cooc_6_refs_sample_dict.p', 'rb'))
 
-WORDS = sorted(list(set(w for ws in COOC_SAMPLES.keys() for w in ws)))
+WORDS_0 = sorted(['mechanism', 'understand', 'explain', 'model', 'theory', 'predict'])
+WORDS_1 = sorted(list(set(w for ws in COOC_SAMPLES.keys() for w in ws)))
+# WORDS_0 = sorted(list(set(w[0] for w in COOC_SAMPLES.keys())))
+# WORDS_1 = sorted(list(set(w[1] for w in COOC_SAMPLES.keys())))
 
 
-def cooc_select(label_text, component_id):
+def cooc_select(label_text, component_id, words):
     return html.Div([
         dbc.Label(label_text, className='content-text', style={'padding-right': '0.5rem'}),
 
         dbc.Select(
             options=[
-                {'label': w, 'value': w} for w in WORDS
+                {'label': w, 'value': w} for w in words
             ],
             id=component_id,
             style={'min-width': '10rem', 'max-width': '20rem'},
@@ -53,8 +54,8 @@ def cooc_entry(i, d):
 coocs_card = dbc.Card([
     dbc.CardHeader([
         html.H3('Références aux cooccurrences', className='mb-3'),
-        cooc_select('Mot 1: ', 'cooc-select-0'),
-        cooc_select('Mot 2: ', 'cooc-select-1'),
+        cooc_select('Mot 1: ', 'cooc-select-0', WORDS_0),
+        cooc_select('Mot 2: ', 'cooc-select-1', WORDS_1),
     ], className='content-card-head'),
     dbc.CardBody([
         html.Div(id='cooc-refs-div')
